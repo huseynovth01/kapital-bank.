e5fd5761f45eca646a1ffa4d9d6b1c7b271b1dab
mac book pro <macbookpro@macs-MacBook-Pro.local>
2024-01-15 18:49:19 +0400

    restart

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-// let form_group = document.querySelector(".form-group");
-// let phoneNumber = document.querySelector(".phoneNumber");
-// let countryCode = document.querySelector("#countryCode");
-// let form_control = document.querySelector(".form-control");
-// let sumbit = document.querySelector(".sumbit");
-let appealsForm = document.querySelector("#appealsForm");
+let form_group = document.querySelector(".form-group");
+let phoneNumber = document.querySelector(".phoneNumber");
+let countryCode = document.querySelector("#countryCode");
+let form_control = document.querySelector(".form-control");
+let sumbit = document.querySelector(".sumbit");
+
+
+
+
+
 
 const baseUrl = "https://jsonplaceholder.typicode.com/posts";
 
@@ -29,5 +33,31 @@ function submit (e){
         console.log(error);
     })
 }
-form.addEventListener("submit",)
+form.addEventListener("sumbit",)
 
+
+//error message
+
+let appealsForm = document.querySelector("firstform");
+
+let name = document.querySelector(".namemessage");
+let surname = document.querySelector(".surnamemessage");
+let father = document.querySelector(".fathernamemessage");
+
+document.querySelector("firstform").addEventListener("sumbit",function(e){
+    e.preventDefault();
+
+    if(formcontrol.value.trim()===""){
+    
+        namemessage.textContent = "Zehmet olmasa xanani bos saxlamayin"
+        namemessage.style.color = "red"
+        appealsForm.style.border = "1px solid red"
+     
+     }
+     else{
+        namemessage.textContent = ""
+        namemessage.style.border = "1 px solid black"
+         
+     }
+
+});
