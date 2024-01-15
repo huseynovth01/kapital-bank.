803b0d62b1c266568e507e9e7d0616212781b156
mac book pro <macbookpro@macs-MacBook-Pro.local>
2024-01-15 16:59:35 +0400

    restart

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// let form_group = document.querySelector(".form-group");
+// let phoneNumber = document.querySelector(".phoneNumber");
+// let countryCode = document.querySelector("#countryCode");
+// let form_control = document.querySelector(".form-control");
+// let sumbit = document.querySelector(".sumbit");
+let appealsForm = document.querySelector("#appealsForm");
+
+const baseUrl = "https://jsonplaceholder.typicode.com/posts";
+
+function submit (e){
+    e.preventDefault();
+    let form = this
+    let FormDta = new FormDta(form)
+
+    fetch(baseUrl, {
+
+        method: "POST",
+        body: FormData
+    }).then(response =>{
+        if (response.ok) {
+            return response.json();
+        }
+        else{
+            form.innerHTML = "Error: "
+        }
+    })
+    .catch(error =>  {
+
+        console.log(error);
+    })
+}
+form.addEventListener("submit",)
+
